a0acf63cda9918a2b5fd44bd78fe59213d1952dc
Robert Lieck (@ThinkPet) <robert@ThinkPet.robotics.tu-berlin.de>
2015-05-20 17:23:35 +0200

    fix: add virtual specifier

--- AbstractTemporallyExtendedModel.h
+++ AbstractTemporallyExtendedModel.h
@@ -25,48 +25,48 @@ public:
     virtual ~AbstractTemporallyExtendedModel() = default;
     /**
      * Set the strength of the L1-regularization. */
-    AbstractTemporallyExtendedModel & set_regularization(double) = 0;
+    virtual AbstractTemporallyExtendedModel & set_regularization(double) = 0;
     /**
      * Set the data to be used for learning the model. */
-    AbstractTemporallyExtendedModel & set_data(const data_t &) = 0;
+    virtual AbstractTemporallyExtendedModel & set_data(const data_t &) = 0;
     /**
      * Set the horizon extension. This is the number of time steps the model is
      * extended into the past with each iteration. */
-    AbstractTemporallyExtendedModel & set_horizon_extension(int n) = 0;
+    virtual AbstractTemporallyExtendedModel & set_horizon_extension(int n) = 0;
     /**
      * Set the maximum extension of the model. Data lying more than \p n steps
      * in the past will not be taken into accout for learning the model or
      * making predictions. */
-    AbstractTemporallyExtendedModel & set_maximum_horizon(int n) = 0;
+    virtual AbstractTemporallyExtendedModel & set_maximum_horizon(int n) = 0;
     /**
      * Optimize the model. */
-    double optimize() = 0;
+    virtual double optimize() = 0;
     /**
      * Get a prediction. The data are assumed to represent the past except for
      * the last data point. The function return a probability for the
      * observation and reward from the last data point given the action from the
      * last data point and the history from the rest of the data. */
-    double get_prediction(const data_t & data) = 0;
+    virtual double get_prediction(const data_t & data) = 0;
     /**
      * If in the inner loop the objective gradient is below this value
      * optimization is stopped. */
-    AbstractTemporallyExtendedModel & set_gradient_threshold(double) = 0;
+    virtual AbstractTemporallyExtendedModel & set_gradient_threshold(double) = 0;
     /**
      * If in the inner loop the largest change of the parameters is below this
      * value optimization is stopped. */
-    AbstractTemporallyExtendedModel & set_parameter_threshold(double) = 0;
+    virtual AbstractTemporallyExtendedModel & set_parameter_threshold(double) = 0;
     /**
      * If the number of iterations in the inner loop reaches this value
      * optimization is stopped. */
-    AbstractTemporallyExtendedModel & set_max_inner_loop_iterations(int) = 0;
+    virtual AbstractTemporallyExtendedModel & set_max_inner_loop_iterations(int) = 0;
     /**
      * If the number of iterations in the outer loop reaches this value
      * optimization is stopped. */
-    AbstractTemporallyExtendedModel & set_max_outer_loop_iterations(int) = 0;
+    virtual AbstractTemporallyExtendedModel & set_max_outer_loop_iterations(int) = 0;
     /**
      * If in the outer loop the likelihood changes less than this value
      * optimization is stopped. */
-    AbstractTemporallyExtendedModel & set_likelihood_threshold(double) = 0;
+    virtual AbstractTemporallyExtendedModel & set_likelihood_threshold(double) = 0;
 };
 
 #endif /* ABSTRACT_TEMPORALLY_EXTENDED_MODEL_H_ */
